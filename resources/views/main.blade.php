bcf197e729dcd787c7ec83676c5f8daabf6dd454
fakhrurraziandi <fakhrurrazi.andi@gmail.com>
2020-03-27 13:28:04 +0700

    add google map embedded script input in settings

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -282,7 +282,7 @@ use App\ClientTestimonial;
 
                     </div>
                     <div class="col-md-6 h-100 px-0" style="height:  100vh !important;">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.5254036543893!2d107.65880851532476!3d-6.947178369942726!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e8227918c261%3A0x3936e8e0a71663e3!2sJl.%20Jupiter%20Tengah%20III%2C%20Sekejati%2C%20Kec.%20Buahbatu%2C%20Kota%20Bandung%2C%20Jawa%20Barat%2040286!5e0!3m2!1sen!2sid!4v1584994705283!5m2!1sen!2sid" frameborder="0" style="border:0; height: 100%; width: 100%;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                        {!!setting('contact_section__google_map_embedded_script')!!}
                     </div>
                 </div>
             </div>
